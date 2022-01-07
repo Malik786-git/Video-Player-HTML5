8363cb789f8b2647d6f2f9c7cb877aca355e919f
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2022-01-08 00:01:31 +0500

    add script

--- script.js
+++ script.js
@@ -1,2 +1,53 @@
+// Get DOM element
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+///////////////////////////////////////////////////////
+
+// for clicking on video;
+const toggleVideoStatus = ()=>{
+
+}
+
+// for updating the play / pause icons;
+const updatePlayIcon = ()=>{
+
+}
+// for update the progress bar;
+
+
+const updateProgress = ()=>{
+
+}
+
+// for stop the video;
+
+const stopVideo = ()=>{
+
+}
+
+// 
+////////////////////////////////////////////////////////
+// event listeners for video player....
+video.onclick = toggleVideoStatus();  
+video.onpause = updatePlayIcon();
+video.onplay = updatePlayIcon();
+video.ontimeupdate = updateProgress();
+
+
+// event listeners for buttons
+play.onclick = toggleVideoStatus();
+stop.onclick = stopVideo();
+progress.onchange = setVideoProgress();
+
+
+
+
+
+
+
+
 
 
